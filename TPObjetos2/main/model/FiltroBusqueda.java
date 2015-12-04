7715a01bd82a7c7a7e16c7953e2c457f8685104b
LDAsoft <marcos_alvarenga9@hotmail.com>
2015-12-04 00:22:42 -0300

    Ya tengo sueño, lo que si, y se que hay otras cosas mas importantes pero no me convence el buscar del composite. Pero mañana o luego lo vemos si terminamos todo y queda para mejorar.

--- TPObjetos2/main/model/FiltroBusqueda.java
+++ TPObjetos2/main/model/FiltroBusqueda.java
@@ -3,5 +3,10 @@ package model;
 import java.util.List;
 
 public abstract class FiltroBusqueda {
+	/*
+	 * El Propósito de aplicar este patron de diseño(composite), es para poder componer los 
+	 * parámetros de busqueda con filtros simples y en nuestro caso, es excluyente que se 
+	 * cumplan todos los filtros solcitados.
+	 */
 	public abstract List<Hotel> buscar(List<Hotel> lHoteles);
 }
